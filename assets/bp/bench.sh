0717ce963e13e37fdc0d7948d462f365b017e86d
Ajay MT <ajay.tatachar@gmail.com>
2022-03-17 13:39:31 -0500

    echo newline after each run

--- assets/bp/bench.sh
+++ assets/bp/bench.sh
@@ -6,10 +6,12 @@ echo "testing consistent branches"
 for i in {0..10}
 do
   time -p bp-same
+  echo ""
 done
 
 echo "testing random branches"
 for i in {0..10}
 do
   time -p bp-random
+  echo ""
 done
