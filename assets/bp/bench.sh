9abd340b8fcf80dc575977ad80ee65ca3a042cee
Ajay MT <ajay.tatachar@gmail.com>
2022-03-17 14:00:48 -0500

    dont print posix formatted times

--- assets/bp/bench.sh
+++ assets/bp/bench.sh
@@ -5,13 +5,13 @@ make
 echo "testing consistent branches"
 for i in {0..10}
 do
-  time -p bp-same
+  time bp-same
   echo ""
 done
 
 echo "testing random branches"
 for i in {0..10}
 do
-  time -p bp-random
+  time bp-random
   echo ""
 done
